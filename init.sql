e1aa864ba3b7d27b890e9e9615a042ae8d805f5c
user <raian@newscred.com>
2021-08-29 13:43:13 +0600

    Refactor docker-compose

--- init.sql
+++
@@ -1,6 +0,0 @@
-use mysql;
-CREATE DATABASE IF NOT EXISTS `shoreapp`;
-CREATE DATABASE IF NOT EXISTS `shoreapp_test`;
-CREATE USER 'shore'@'%' IDENTIFIED BY 'qwe90qwe';
-GRANT ALL ON *.* TO 'shore'@'%';
-flush privileges;
---
+++ runner.sh
@@ -0,0 +1,2 @@
+python3 create_database.py
+python3 -m shore_app.app
